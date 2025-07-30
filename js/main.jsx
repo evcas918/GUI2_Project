42fc33ffd68cb425dc81cbfa18bba9caa41315f8
Anh Nguyen <anhnguyen830@hotmail.com>
2025-07-30 17:38:16 -0400

    Update main.jsx

--- js/main.jsx
+++ js/main.jsx
@@ -2,11 +2,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import WorkSpace from './Pages/WorkSpace.jsx'
+import LogIn from './LogIn.jsx'
+import CreateNewAccount from './CreateNewAccount.jsx'
 import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
 
 const router = createBrowserRouter([
   {path:"/", element:<App/>},
   {path:"/workspace", element:<WorkSpace/>},
+  {path:"/log-in", element:<LogIn/>},
+  {path:"/new account", element:<CreateNewAccount/>},
 ]);
 
 createRoot(document.getElementById('root')).render(
