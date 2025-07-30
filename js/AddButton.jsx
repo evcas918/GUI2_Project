5edc56d8bf2b739888aefdb175ca266e58e31783
Anh Nguyen <anhnguyen830@hotmail.com>
2025-07-30 17:24:33 -0400

    Updated Component

--- js/AddButton.jsx
+++ js/AddButton.jsx
@@ -5,11 +5,11 @@ import { Link } from 'react-router-dom'
 function AddButton() {
     return (
         <div id="add-container">
-        <Link to ="/workspace">
-        <button>
-        <img src={addbutton} alt="Add Button" width="50" height="50"/>
-        </button>
-        </Link>
+            <Link to ="/workspace">
+                <button  style={{float: 'right'}}>
+                    <img src={addbutton} alt="Add Button" width="50" height="50"/>
+                </button>
+            </Link>
         </div>
     );
 }
